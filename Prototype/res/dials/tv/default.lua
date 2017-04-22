55cd0595508ae43f2c5826c797aa7ea96bf5c8f8
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-04-22 13:04:15 +0900

    fixed typo

--- Prototype/res/dials/tv/default.lua
+++ Prototype/res/dials/tv/default.lua
@@ -1,13 +1,13 @@
 local dialog = {
 	{
-		text = 'Why not watch what\'s new in Kardashians\'s lives',
+		text = 'Why not watch what\'s new in Kardashians\'s lives ?',
 		answers = {
 			{
-				text = 'Just kidding, this is shit.',
-				response = function(e1,e2) e2.isLate = true end
+				text = 'Just kidding, this is shit.'
 			},
 			{
-				text = 'Okay just five minutes.'
+				text = 'Okay just five minutes.',
+				response = function(e1, e2) print('You\'re dumb') e2.isLate = true end
 			}
 		}
 	}
