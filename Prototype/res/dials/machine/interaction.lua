05fea05f57af50b07da6e16ba9babcae5f4aa966
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-04-24 21:52:42 +0900

    reknown callback

--- Prototype/res/dials/machine/interaction.lua
+++ Prototype/res/dials/machine/interaction.lua
@@ -3,10 +3,12 @@ return {
         text = "Pr. Noname : Yes this machine ! Please push the right button to start.",
         answers =  {
           {
-            text = "Let's push the left button."
+            text = "Let's push the left button.",
+            callback = function(player, scene) player.brokeTheMachine = true end
           },
           {
-            text = "He said the 'right' one, let's push right button."
+            text = "He said the 'right' one, let's push right button.",
+            callback = function(player, scene) player.brokeTheMachine = true end
           }
         }
     }
