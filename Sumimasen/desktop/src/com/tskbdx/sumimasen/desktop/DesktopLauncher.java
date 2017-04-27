a304fdcd8d87c7aafd554ddbd84efac80f946bf5
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-04-27 14:41:46 +0900

    Empty project for game

---
+++ Sumimasen/desktop/src/com/tskbdx/sumimasen/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tskbdx.sumimasen.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tskbdx.sumimasen.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
