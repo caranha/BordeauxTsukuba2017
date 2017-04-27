1cbb38114d0d57f6829a6fa5e28fd8ef983523ca
sydpy <sylvain.dupouy@etu.u-bordeaux.fr>
2017-04-27 15:58:17 +0900

    Game.java --> Sumimasen.java

--- Sumimasen/desktop/src/com/tskbdx/sumimasen/desktop/DesktopLauncher.java
+++ Sumimasen/desktop/src/com/tskbdx/sumimasen/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.tskbdx.sumimasen.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.tskbdx.sumimasen.Game;
+import com.tskbdx.sumimasen.Sumimasen;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new Sumimasen(), config);
 	}
 }
