872026250712dd8316e9b4c6bb1a5b4d4e715302
Viet Khang Le Ho <viet-khang.le-ho@etu.u-bordeaux.fr>
2017-05-09 04:02:26 +0200

    Merge

---
+++ Sumimasen/core/src/com/tskbdx/sumimasen/scenes/view/entities/Animation.java
@@ -0,0 +1,12 @@
+package com.tskbdx.sumimasen.scenes.view.entities;
+
+/**
+ * Created by Sydpy on 5/6/17.
+ */
+public interface Animation {
+
+    void start();
+    void update();
+
+    boolean isFinished();
+}
---
+++ Sumimasen/core/src/com/tskbdx/sumimasen/scenes/view/entities/PositionInterpolationAnimation.java
@@ -0,0 +1,61 @@
+package com.tskbdx.sumimasen.scenes.view.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Sydpy on 5/6/17.
+ */
+public class PositionInterpolationAnimation implements Animation {
+
+    private Vector2 start, target;
+    private final float duration;
+
+    private Rectangle rectangle;
+
+    private boolean started = false;
+    private boolean running = false;
+
+    private float clock = 0.f;
+
+    public PositionInterpolationAnimation(Rectangle rectangle, Vector2 target, float duration) {
+
+        this.start      = new Vector2(rectangle.x, rectangle.y);
+        this.target     = target;
+        this.duration   = duration;
+
+        this.rectangle = rectangle;
+    }
+
+    @Override
+    public void start() {
+        started = true;
+        running = true;
+    }
+
+    @Override
+    public void update() {
+        if(started && running) {
+
+            float dt = Gdx.graphics.getDeltaTime();
+            clock += dt;
+
+            float progress = Math.min(1.f, clock/duration);
+
+            rectangle.setPosition(
+                    start.interpolate(target, progress, Interpolation.linear)
+            );
+
+            if (progress == 1.f) {
+                running = false;
+            }
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return started && !running;
+    }
+}
