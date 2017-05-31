d48641c81c7f782f24c4cc536042fba633d75b64
Viet Khang Le Ho <viet-khang.le-ho@etu.u-bordeaux.fr>
2017-05-31 17:31:47 +0200

    add repeat in Utility

--- Sumimasen/core/src/com/tskbdx/sumimasen/scenes/story/scriptIntroScene/LateOnFirstDay.java
+++ Sumimasen/core/src/com/tskbdx/sumimasen/scenes/story/scriptIntroScene/LateOnFirstDay.java
@@ -1,8 +1,17 @@
 package com.tskbdx.sumimasen.scenes.story.scriptIntroScene;
 
 import com.tskbdx.sumimasen.scenes.Scene;
+import com.tskbdx.sumimasen.scenes.model.World;
+import com.tskbdx.sumimasen.scenes.model.entities.Direction;
+import com.tskbdx.sumimasen.scenes.model.entities.Entity;
+import com.tskbdx.sumimasen.scenes.model.entities.movements.Path;
+import com.tskbdx.sumimasen.scenes.model.entities.movements.Walk;
 import com.tskbdx.sumimasen.scenes.story.Event;
 import com.tskbdx.sumimasen.scenes.story.State;
+import com.tskbdx.sumimasen.scenes.utility.Utility;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /*
  * Created by viet khang on 31/05/2017.
@@ -10,6 +19,15 @@ import com.tskbdx.sumimasen.scenes.story.State;
 class LateOnFirstDay implements State {
     @Override
     public void process(Scene scene) {
+        World world = scene.getWorld();
+        Entity noname = world.getEntityByName("Pr. Noname");
+
+        List<Direction> path = new LinkedList<>();
+        Utility.repeat(() -> path.add(Direction.WEST), 7);
+        Utility.repeat(() -> path.add(Direction.SOUTH), 4);
+
+        noname.setMovement(new Path(false, Direction.WEST));
+
         System.out.println("LATE !");
     }
 
