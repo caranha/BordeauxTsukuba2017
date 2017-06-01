1273dac7d9e7df3324e2e2e5f60298443e600db8
Viet Khang Le Ho <viet-khang.le-ho@etu.u-bordeaux.fr>
2017-06-01 18:33:39 +0200

    rename go get backu up to break machine scene

---
+++ Sumimasen/core/src/com/tskbdx/sumimasen/scenes/Restaurant.java
@@ -0,0 +1,48 @@
+package com.tskbdx.sumimasen.scenes;
+
+import com.tskbdx.sumimasen.GameScreen;
+import com.tskbdx.sumimasen.scenes.model.entities.Entity;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/*
+ * Created by viet khang on 01/06/2017.
+ */
+public class Restaurant extends Scene {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        Entity player = GameScreen.getPlayer();
+        return player.hasTag("fed");
+    }
+
+    @Override
+    public Scene getNextScene() {
+        return null;
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    protected String defaultMap() {
+        return null;
+    }
+
+    @Override
+    protected String defaultSpawn() {
+        return null;
+    }
+
+    @Override
+    public String description() {
+        boolean randomBool = ThreadLocalRandom.current().nextBoolean();
+        return randomBool ? "Hum I feel so hungry..." : "I heard that a new restaurant opened recently";
+    }
+}
