b8013405a70241a6cd1bf4f010bc3c274069d45b
Viet Khang Le Ho <viet-khang.le-ho@etu.u-bordeaux.fr>
2017-06-01 11:49:31 +0200

    maj dialogue default letswork

---
+++ Sumimasen/core/src/com/tskbdx/sumimasen/scenes/LetsWork.java
@@ -0,0 +1,50 @@
+package com.tskbdx.sumimasen.scenes;
+
+import com.tskbdx.sumimasen.scenes.model.World;
+import com.tskbdx.sumimasen.scenes.model.entities.Entity;
+import com.tskbdx.sumimasen.scenes.model.entities.interactions.Dialogue;
+
+/*
+ * Created by viet khang on 01/06/2017.
+ */
+public class LetsWork extends Scene {
+    @Override
+    public void init() {
+        World world = getWorld();
+        Entity noname = world.getEntityByName("Pr. Noname");
+        Entity machine = world.getEntityByName("Machine");
+
+        machine.setInteraction(new Dialogue("letsWork.xml"));
+        noname.setInteraction(new Dialogue("default.xml"));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public Scene getNextScene() {
+        return null;
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    protected String defaultMap() {
+        return "lab";
+    }
+
+    @Override
+    protected String defaultSpawn() {
+        return "left_entrance";
+    }
+
+    @Override
+    protected String description() {
+        return null;
+    }
+}
